7c8b8329c43c683a7661631a66b7fbc7ec950cb5
temp <temp@oye-a01.vmware.com>
2020-11-09 12:15:57 +0800

    add fake script and makefile

---
+++ run.sh
@@ -0,0 +1,4 @@
+emacs ./index.org -f org-html-export-to-html --kill
+emacs ./shell/index.org -f org-html-export-to-html --kill
+emacs ./shell/example.org -f org-html-export-to-html --kill
+emacs ./shell/cookbook.org -f org-html-export-to-html --kill
