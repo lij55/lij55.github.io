c2dcb16500bf80d3ab8fbbf24bb573b6573048b4
temp <temp@oye-a01.vmware.com>
2020-11-18 21:21:14 +0800

    more base tips

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -sf `pwd`/note.setup shell/
-emacs ./index.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
-emacs ./shell/index.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
-emacs ./shell/tools.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
-emacs ./shell/cookbook.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs  ./index.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs  ./shell/index.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs  ./shell/tools.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs  ./shell/cookbook.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
