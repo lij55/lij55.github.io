4ca71718a91d9ee4cf2ac485fe414cbab42addea
Jasper Li <jasli@pivotal.io>
2020-11-18 15:57:00 +0800

    fix the batch issue. Now it is generated automatically

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-emacs ./index.org -f org-html-export-to-html --kill
-emacs ./shell/index.org -f org-html-export-to-html --kill
-emacs ./shell/tools.org -f org-html-export-to-html --kill
-emacs ./shell/cookbook.org -f org-html-export-to-html --kill
+#!/bin/bash
+
+ln -sf `pwd`/note.setup shell/
+emacs ./index.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs ./shell/index.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs ./shell/tools.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
+emacs ./shell/cookbook.org --batch -l ~/.emacs.d/init.el -f org-html-export-to-html --kill
