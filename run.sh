66d341fcb2cfb025a7dfab113d8c34b2762f4b8b
Jasper Li <jasli@pivotal.io>
2020-11-18 15:07:03 +0800

    update html to relative path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 emacs ./index.org -f org-html-export-to-html --kill
 emacs ./shell/index.org -f org-html-export-to-html --kill
-emacs ./shell/example.org -f org-html-export-to-html --kill
+emacs ./shell/tools.org -f org-html-export-to-html --kill
 emacs ./shell/cookbook.org -f org-html-export-to-html --kill
